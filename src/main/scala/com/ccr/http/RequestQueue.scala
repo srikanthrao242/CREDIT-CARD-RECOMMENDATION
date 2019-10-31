f724d20cbea07f095833a110f71ef0bb9d371c2f
joshi srikanth rao <srikanthraomca@yahoo.com>
2019-10-31 21:14:26 +0530

    Update RequestQueue.scala

--- src/main/scala/com/ccr/http/RequestQueue.scala
+++ src/main/scala/com/ccr/http/RequestQueue.scala
@@ -13,35 +13,61 @@ import scala.util.{Failure, Success}
 
 trait RequestQueue {
 
-  implicit val actorSystem: ActorSystem
-  implicit val actorMaterializer: ActorMaterializer
+  val logger = Logger.getLogger(getClass.getName)
+  implicit val system: ActorSystem
+  implicit val materializer: ActorMaterializer
   val host: String
   val queueSize: Int
+  private lazy val poolClientFlow =
+    Http().cachedHostConnectionPoolHttps[Promise[HttpResponse]](host, 443)
 
-  val logger = Logger.getLogger(this.getClass.getName)
-  val poolClientFlow = Http()
-    .cachedHostConnectionPool[Promise[HttpResponse]](host,443)
-  val queue =
-    Source.queue[(HttpRequest, Promise[HttpResponse])](
-      queueSize, OverflowStrategy.dropNew
-    )
+  private lazy val queue =
+    Source
+      .queue[(HttpRequest, Promise[HttpResponse])](queueSize,
+                                                   OverflowStrategy.dropNew)
+      .map { requestResponsePair =>
+        logger.info(s"Http Request : ${requestResponsePair._1}}")
+        requestResponsePair
+      }
       .via(poolClientFlow)
       .toMat(Sink.foreach({
-        case ((Success(resp), p)) => p.success(resp)
-        case ((Failure(e), p))    => p.failure(e)
+        case (Success(resp), p) =>
+          p.success(resp)
+          ()
+        case (Failure(resp), p) =>
+          p.failure(resp)
+          ()
       }))(Keep.left)
       .run()
 
-  def queueRequest(request: HttpRequest)(
-    implicit ec: ExecutionContext
-  ): Future[HttpResponse] = {
+  def queueRequest(
+    request: HttpRequest,
+    retries: Int
+  )(implicit
+    ec: ExecutionContext): Future[HttpResponse] = {
     val responsePromise = Promise[HttpResponse]()
-    queue.offer(request -> responsePromise).flatMap {
-      case QueueOfferResult.Enqueued    => responsePromise.future
-      case QueueOfferResult.Dropped     => Future.failed(new RuntimeException("Queue overflowed. Try again later."))
-      case QueueOfferResult.Failure(ex) => Future.failed(ex)
-      case QueueOfferResult.QueueClosed => Future.failed(new RuntimeException("Queue was closed (pool shut down) while running the request. Try again later."))
-    }
+    queue
+      .offer(request -> responsePromise)
+      .flatMap {
+        case QueueOfferResult.Enqueued => responsePromise.future
+        case QueueOfferResult.Dropped =>
+          Future
+            .failed(new RuntimeException("Queue overflowed. Try again later."))
+        case QueueOfferResult.Failure(ex) => Future.failed(ex)
+        case QueueOfferResult.QueueClosed =>
+          Future.failed(
+            new RuntimeException(
+              "Queue was closed (pool shut down) while running the request. Try again later."
+            )
+          )
+      }
+      .recoverWith {
+        case e: Exception if retries == 0 =>
+          throw new Exception(s"${e.getMessage}")
+        case e: Exception if retries > 0 =>
+          logger.info(s"Unexpected connection closure, retries left: $retries")
+          queueRequest(request, retries - 1)
+      }
   }
 }
 
@@ -49,9 +75,10 @@ object RequestQueue {
   def apply(_host: String, _queueSize: Int)(
     implicit _actorSystem: ActorSystem,
     _actorMaterialize: ActorMaterializer
-  ): RequestQueue = new RequestQueue{
-    override implicit val actorMaterializer: ActorMaterializer = _actorMaterialize
-    override implicit val actorSystem: ActorSystem = _actorSystem
+  ): RequestQueue = new RequestQueue {
+    override implicit val system: ActorSystem = _actorSystem
+    override implicit val materializer: ActorMaterializer =
+      _actorMaterialize
     override val host: String = _host
     override val queueSize: Int = _queueSize
   }
